4bccc7a8a40779a23632c9b72ac65574778ab9e4
Vittorio Masia <14254644+MasiaVittorio@users.noreply.github.com>
2025-04-17 18:04:21 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/scrollable_card/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.scrollable_card
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/scrollable_card.dart
@@ -0,0 +1,326 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:sid_base/sid_base.dart';
+
+typedef ScrolledBuilder =
+    Widget Function(BuildContext context, VoidCallback unscroll);
+typedef UnscrolledBuilder =
+    Widget Function(
+      BuildContext context,
+      VoidCallback scrollFromRight,
+      VoidCallback scrollFromLeft,
+    );
+
+class ScrollableCard extends StatefulWidget {
+  const ScrollableCard({
+    super.key,
+    this.backgroundColor,
+    this.scrolledBackgroundColor,
+    this.shape = const RoundedRectangleBorder(),
+    this.scrolledShape = const RoundedRectangleBorder(),
+    required this.builder,
+    this.fromRightBuilder,
+    this.fromLeftBuilder,
+    this.scrolledMargin = EdgeInsets.zero,
+    this.margin = EdgeInsets.zero,
+  });
+
+  final Color? backgroundColor;
+  final Color? scrolledBackgroundColor;
+  final ShapeBorder shape;
+  final ShapeBorder scrolledShape;
+  final UnscrolledBuilder builder;
+  final ScrolledBuilder? fromRightBuilder;
+  final ScrolledBuilder? fromLeftBuilder;
+  final EdgeInsets margin;
+  final EdgeInsets scrolledMargin;
+
+  @override
+  State<ScrollableCard> createState() => _ScrollableCardState();
+}
+
+enum _CloseTo { center, scrollFromRight, scrollFromLeft }
+
+enum _Going { fastFromLeft, fastFromRight, slow }
+
+enum _Started { center, scrollFromRight, scrollFromLeft }
+
+class _ScrollableCardState extends State<ScrollableCard>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController controller = AnimationController(
+    vsync: this,
+    lowerBound: -1,
+    upperBound: 1,
+    value: 0,
+    duration: Durations.long1,
+  );
+
+  bool get allowFromRight => widget.fromRightBuilder != null;
+  bool get allowFromLeft => widget.fromLeftBuilder != null;
+
+  void finishScrollFromRight() {
+    if (!mounted) return;
+    if (!allowFromRight) return;
+    controller.animateTo(
+      1.0,
+      curve: Easings.emphasized,
+      duration: Durations.medium3,
+    );
+  }
+
+  void finishScrollFromLeft() {
+    if (!mounted) return;
+    if (!allowFromLeft) return;
+    controller.animateTo(
+      -1.0,
+      curve: Easings.emphasized,
+      duration: Durations.long1,
+    );
+  }
+
+  void unscroll() {
+    if (!mounted) return;
+    controller.animateBack(
+      0.0,
+      curve: Easings.emphasized,
+      duration: Durations.long1,
+    );
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  double lastStartingValue = 0.0;
+  Offset lastStartingOffset = Offset.zero;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = context.theme;
+    final backgroundColor =
+        widget.backgroundColor ?? theme.colorScheme.surfaceContainerLow;
+    final scrolledBackgroundColor =
+        widget.scrolledBackgroundColor ??
+        theme.colorScheme.surfaceContainerHighest;
+
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final w = constraints.maxWidth;
+        final originalWidth = w - widget.margin.horizontal;
+        return ConstrainedBox(
+          constraints: constraints,
+          child: AnimatedBuilder(
+            animation: controller,
+            builder: (context, child) {
+              final value = controller.value.abs();
+              var shape =
+                  ShapeBorder.lerp(widget.shape, widget.scrolledShape, value)!;
+              return Padding(
+                padding:
+                    EdgeInsets.lerp(
+                      widget.margin,
+                      widget.scrolledMargin,
+                      value,
+                    )!,
+                child: DecoratedBox(
+                  position: DecorationPosition.foreground,
+                  decoration: ShapeDecoration(shape: shape),
+                  child: Container(
+                    clipBehavior: Clip.antiAlias,
+                    decoration: ShapeDecoration(
+                      shape: shape,
+                      color:
+                          Color.lerp(
+                            backgroundColor,
+                            scrolledBackgroundColor,
+                            value,
+                          )!,
+                    ),
+                    child: child!,
+                  ),
+                ),
+              );
+            },
+            child: GestureDetector(
+              onPanStart: (details) {
+                lastStartingOffset = details.localPosition;
+                lastStartingValue = controller.value;
+              },
+              onPanUpdate: (details) {
+                final Offset delta = details.localPosition - lastStartingOffset;
+                final dx = delta.dx;
+                final df = dx / w;
+                final sign = -df.sign;
+                final tdf = 1 - pow(2, -df.abs() * 10);
+                final stdf = tdf * sign;
+                controller.value = (lastStartingValue + stdf).clamp(
+                  allowFromLeft ? -1 : 0,
+                  allowFromRight ? 1 : 0,
+                );
+              },
+              onPanEnd: (details) {
+                final double vx = details.velocity.pixelsPerSecond.dx;
+                final _Started started = switch (lastStartingValue) {
+                  < -0.1 => _Started.scrollFromLeft,
+                  > 0.1 => _Started.scrollFromRight,
+                  _ => _Started.center,
+                };
+
+                const threshold = 750.0;
+                final _Going going = switch (vx) {
+                  < -threshold => _Going.fastFromRight,
+                  > threshold => _Going.fastFromLeft,
+                  _ => _Going.slow,
+                };
+                final _CloseTo closeTo = switch (controller.value) {
+                  < -0.75 => _CloseTo.scrollFromLeft,
+                  > 0.75 => _CloseTo.scrollFromRight,
+                  _ => _CloseTo.center,
+                };
+                switch ((started, going, closeTo)) {
+                  case (
+                        _Started.scrollFromRight || _Started.center,
+                        _Going.fastFromRight,
+                        _,
+                      ) ||
+                      (
+                        _Started.center || _Started.scrollFromRight,
+                        _Going.slow,
+                        _CloseTo.scrollFromRight,
+                      ):
+                    finishScrollFromRight();
+                    return;
+                  case (
+                        _Started.scrollFromLeft || _Started.center,
+                        _Going.fastFromLeft,
+                        _,
+                      ) ||
+                      (
+                        _Started.center || _Started.scrollFromLeft,
+                        _Going.slow,
+                        _CloseTo.scrollFromLeft,
+                      ):
+                    finishScrollFromLeft();
+                    return;
+                  case (_, _Going.slow, _CloseTo.center) ||
+                      (_Started.scrollFromRight, _Going.fastFromLeft, _) ||
+                      (_Started.scrollFromLeft, _Going.fastFromRight, _) ||
+                      (
+                        _Started.scrollFromRight,
+                        _Going.slow,
+                        _CloseTo.scrollFromLeft,
+                      ) ||
+                      (
+                        _Started.scrollFromLeft,
+                        _Going.slow,
+                        _CloseTo.scrollFromRight,
+                      ):
+                    unscroll();
+                    return;
+                }
+              },
+              child: Container(
+                color: Colors.transparent, // for the gesture
+                child: Stack(
+                  children: [
+                    AnimatedBuilder(
+                      animation: controller,
+                      builder: (context, child) {
+                        final value = controller.value;
+                        return Transform.translate(
+                          offset: Offset(
+                            -value.rangeMapLoose(to: (0, w / 4)),
+                            0,
+                          ),
+                          child: Opacity(
+                            opacity: (1 - value.abs()).rangeMap(
+                              from: (0.45, 1),
+                            ),
+                            child: IgnorePointer(
+                              ignoring: value.abs() > 0.1,
+                              child: child!,
+                            ),
+                          ),
+                        );
+                      },
+                      child: SizedBox(
+                        width: originalWidth,
+                        child: Material(
+                          type: MaterialType.transparency,
+                          child: widget.builder(
+                            context,
+                            finishScrollFromRight,
+                            finishScrollFromLeft,
+                          ),
+                        ),
+                      ),
+                    ),
+                    if (widget.fromRightBuilder
+                        case ScrolledBuilder fromRightBuilder)
+                      Positioned.fill(
+                        child: AnimatedBuilder(
+                          animation: controller,
+                          builder: (context, child) {
+                            final value =
+                                controller.value.clamp(0.0, 1.0).abs();
+                            return Transform.translate(
+                              offset: Offset(
+                                value.rangeMapLoose(to: (w / 4, 0)),
+                                0,
+                              ),
+                              child: Opacity(
+                                opacity: value.rangeMap(from: (0.55, 1)),
+                                child: IgnorePointer(
+                                  ignoring: value.abs() < 0.9,
+                                  child: child!,
+                                ),
+                              ),
+                            );
+                          },
+                          child: Material(
+                            type: MaterialType.transparency,
+                            child: fromRightBuilder(context, unscroll),
+                          ),
+                        ),
+                      ),
+                    if (widget.fromLeftBuilder
+                        case ScrolledBuilder fromLeftBuilder)
+                      Positioned.fill(
+                        child: AnimatedBuilder(
+                          animation: controller,
+                          builder: (context, child) {
+                            final value =
+                                controller.value.clamp(-1.0, 0.0).abs();
+                            return Transform.translate(
+                              offset: Offset(
+                                value.rangeMapLoose(to: (-w / 4, 0)),
+                                0,
+                              ),
+                              child: Opacity(
+                                opacity: value.rangeMap(from: (0.55, 1)),
+                                child: IgnorePointer(
+                                  ignoring: value.abs() < 0.9,
+                                  child: child!,
+                                ),
+                              ),
+                            );
+                          },
+                          child: Material(
+                            type: MaterialType.transparency,
+                            child: fromLeftBuilder(context, unscroll),
+                          ),
+                        ),
+                      ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
